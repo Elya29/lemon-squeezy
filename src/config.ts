5cf106a6e001e413a7f7587f635fd8639c1c72b7
Elya29 <elya29870@gmail.com>
2025-02-28 19:15:49 +0100

    fix backlink on /

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import type { Site, Ui, Features, Url } from './types'
 
 export const SITE: Site = {
   website: 'https://elya29.github.io',
-  base: '/lemon-squeezy',
+  base: '/lemon-squeezy/',
   title: 'Lemon Squeezy',
   description: 'La programmation pour les enfants',
   author: 'Alex Palma',
